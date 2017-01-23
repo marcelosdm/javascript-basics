9a3d2b3ba2f59a01f79d7bc1c647b58e6bff488b
Marcelo Melo <marcelosdm@gmail.com>
2017-01-23 08:45:38 -0200

    Add files via upload

---
+++ 3. Trakcing Data Using Objects/challenge/js/students.js
@@ -0,0 +1,32 @@
+var students = [
+{
+	name: 'Marcelo',
+	track: 'Front End Development',
+	achievements: 5,
+	points: 1500
+},
+{
+	name: 'Livia',
+	track: 'Front End Development',
+	achievements: 5,
+	points: 1250
+},
+{
+	name: 'Gabriel',
+	track: 'iOS',
+	achievements: 5,
+	points: 200
+},
+{
+	name: 'Jordan',
+	track: 'Android',
+	achievements: 5,
+	points: 1000
+},
+{
+	name: 'Phill',
+	track: 'Business',
+	achievements: 2,
+	points: 200
+},
+];
